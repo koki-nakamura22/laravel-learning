9614b4d42f99cf4b6ac99524c1f5d24e5b1028da
Koki NAKAMURA <koki.nakamura22@gmail.com>
2022-11-05 16:21:12 +0900

    implements the error handling functions

--- todo/todo-app/app/Providers/AuthServiceProvider.php
+++ todo/todo-app/app/Providers/AuthServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use App\Models\Folder;
+use App\Policies\FolderPolicy;
+
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 
@@ -14,6 +17,7 @@ class AuthServiceProvider extends ServiceProvider
      */
     protected $policies = [
         // 'App\Models\Model' => 'App\Policies\ModelPolicy',
+        Folder::class => FolderPolicy::class,
     ];
 
     /**
